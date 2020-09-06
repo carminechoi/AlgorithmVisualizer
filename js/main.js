9cab89de9cf2139f5ffe3f8fc4585eb30102f583
Carmine <carminec@uci.edu>
2020-09-06 16:13:22 -0700

    fixed update and draw function. Both  independent

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-var x = 0;
-var y = 0;
-var specialX = 0;
-var specialY = 0;
+const ROWS = 25;
+const COLS = 40;
+const allCells = $("#board td");
 
-var isDragging = false;
-var isStart = false;
-var isEnd = false;
-var cellStatus = 0;
-var allCells = $("#board td");
+var Board = {
+	board: createBoard(),
+	x: 0,
+	y: 0,
+	startX: 11,
+	startY: 8,
+	specialPrev: 0,
+	endX: 11,
+	endY: 31,
+	isStart: false,
+	isEnd: false,
+	isSelected: false,
+	isDragging: false,
+};
 
 /* Initialize Board */
-var board = new Array(25);
-for (var i = 0; i < 40; i++) {
-	board[i] = [];
-}
-for (var i = 0; i < board.length; i++) {
-	for (var j = 0; j < board.length; j++) {
-		board[i][j] = 0;
+function createBoard() {
+	var board = new Array(COLS);
+	for (var i = 0; i < ROWS; i++) {
+		board[i] = [];
 	}
+	for (var i = 0; i < ROWS; i++) {
+		for (var j = 0; j < COLS; j++) {
+			board[i][j] = 0;
+		}
+	}
+	return board;
 }
-
 /* Add Start and End Positions */
 var start = "#11-8";
 var end = "#11-31";
-board[11][8] = 2;
-board[11][31] = 3;
+Board.board[11][8] = 2;
+Board.board[11][31] = 3;
 $(start).addClass("start");
 $(end).addClass("end");
 
@@ -34,11 +44,12 @@ $("#board td").mousedown(function rangeMouseDown(e) {
 	if (isRightClick(e)) {
 		return false;
 	} else {
-		isDragging = true;
-		var selected = allCells.index($(this));
-		var coord = indexToCoord(selected);
-		checkIsSpecial();
-		cellStatus = checkCellStatus(coord);
+		Board.isDragging = true;
+		var cell = this;
+		indexToCoord(cell);
+		checkCellStatus();
+		updateBoard();
+		drawTable();
 
 		if (typeof e.preventDefault != "undefined") {
 			e.preventDefault();
@@ -46,21 +57,20 @@ $("#board td").mousedown(function rangeMouseDown(e) {
 		document.documentElement.onselectstart = function () {
 			return false;
 		};
-
-		selectRange(cellStatus, coord);
-		console.log("DOWN - isStart: " + isStart);
-		console.log("DOWN - isEnd  : " + isEnd);
-		console.log("DOWN - status : " + cellStatus);
 	}
 });
 
 $("#board td").mousemove(function rangeMouseMove() {
-	if (isDragging) {
-		var selected = allCells.index($(this));
-		var coord = indexToCoord(selected);
-		var tempStatus = checkCellStatus(coord);
-		if (tempStatus == 0 || tempStatus == 1) {
-			selectRange(cellStatus, coord);
+	if (Board.isDragging) {
+		var cell = this;
+		indexToCoord(cell);
+		//checkCellStatus();
+		if (Board.isEnd == false && Board.isStart == false) {
+			updateBoard();
+			drawTable();
+		} else {
+			updateBoard();
+			drawTable();
 		}
 	}
 });
@@ -69,38 +79,27 @@ $("#board td").mouseup(function rangeMouseUp(e) {
 	if (isRightClick(e)) {
 		return false;
 	} else {
-		isDragging = false;
-		if (isStart) {
-			board[specialX][specialY] = 0;
-			board[x][y] = 2;
-			isStart = false;
-		} else if (isEnd) {
-			board[specialX][specialY] = 0;
-			board[x][y] = 3;
-			isEnd = false;
-		}
+		Board.isDragging = false;
 		document.documentElement.onselectstart = function () {
 			return true;
 		};
-		console.log("UP - isStart: " + isStart);
-		console.log("UP - isEnd  : " + isEnd);
-		console.log(board);
+		resetBoard();
+		console.table(Board.board);
 	}
 });
 
 function selectRange(cellStatus, coord) {
+	drawTable();
 	if (cellStatus == 0) {
 		$(coord).addClass("selected");
-		board[x][y] = 1;
 	} else if (cellStatus == 1) {
-		board[x][y] = 0;
 		$(coord).removeClass("selected");
 	} else if (cellStatus == 2) {
 		$("#board td").removeClass("start");
 		$(coord).addClass("start");
 	} else if (cellStatus == 3) {
 		$("#board td").removeClass("end");
-		$(coord).addClass("end");
+		$(coord).addClass("start");
 	}
 }
 
@@ -112,41 +111,90 @@ function isRightClick(e) {
 	}
 	return false;
 }
+
+function updateBoard() {
+	if (
+		Board.isSelected == false &&
+		Board.isStart == false &&
+		Board.isEnd == false
+	) {
+		Board.board[Board.x][Board.y] = 1;
+	} else if (Board.isSelected) {
+		Board.board[Board.x][Board.y] = 0;
+	} else if (Board.isStart) {
+		Board.board[Board.startX][Board.startY] = Board.specialPrev;
+		Board.specialPrev = Board.board[Board.x][Board.y];
+		Board.startX = Board.x;
+		Board.startY = Board.y;
+		Board.board[Board.x][Board.y] = 2;
+	} else if (Board.isEnd) {
+		Board.board[Board.endX][Board.endY] = Board.specialPrev;
+		Board.specialPrev = Board.board[Board.x][Board.y];
+		Board.endX = Board.x;
+		Board.endY = Board.y;
+		Board.board[Board.x][Board.y] = 3;
+	}
+}
+
+function drawTable() {
+	$("#board td").removeClass("selected");
+	$("#board td").removeClass("start");
+	$("#board td").removeClass("end");
+
+	var row, col;
+	for (row = 0; row < ROWS; row++) {
+		for (col = 0; col < COLS; col++) {
+			var coordString = convertCoordToString(row, col);
+			if (Board.board[row][col] == 1) {
+				$(coordString).addClass("selected");
+			} else if (Board.board[row][col] == 2) {
+				$(coordString).addClass("start");
+			} else if (Board.board[row][col] == 3) {
+				$(coordString).addClass("end");
+			}
+		}
+	}
+}
+
 /************** ___END Mouse Input Functions___ **************/
 
 /********* ___START Mouse Input HELPER Functions___  *********/
-function indexToCoord(index) {
-	x = Math.floor(index / 40);
-	y = index % 40;
+function indexToCoord(cell) {
+	var selected = allCells.index($(cell));
 
+	Board.x = Math.floor(selected / 40);
+	Board.y = selected % 40;
+}
+
+function convertCoordToString(x, y) {
 	var result = "#".concat(x.toString(10), "-", y.toString(10));
-
 	return result;
 }
 
-function checkCellStatus(coord) {
-	if ($(coord).hasClass("selected")) {
-		return 1;
-	} else if ($(coord).hasClass("start")) {
-		return 2;
-	} else if ($(coord).hasClass("end")) {
-		return 3;
-	} else {
-		return 0;
+function checkCellStatus() {
+	var x = Board.x;
+	var y = Board.y;
+
+	if (Board.board[x][y] == 0) {
+		Board.isSelected = false;
+	} else if (Board.board[x][y] == 1) {
+		Board.isSelected = true;
+	} else if (Board.board[x][y] == 2) {
+		Board.isStart = true;
+	} else if (Board.board[x][y] == 3) {
+		Board.isEnd = true;
 	}
+	console.log("isSelected: " + Board.isSelected);
+	console.log("isStart: " + Board.isStart);
+	console.log("isEnd: " + Board.isEnd);
 }
 
-function checkIsSpecial() {
-	if (board[x][y] == 2) {
-		isStart = true;
-		specialX = x;
-		specialY = y;
-	} else if (board[x][y] == 3) {
-		isEnd = true;
-		specialX = x;
-		specialY = y;
-	}
+function resetBoard() {
+	Board.isStart = false;
+	Board.isEnd = false;
+	Board.isSelected = false;
 }
+
 /*********** ___END Mouse Input HELPER Functions___ **********/
 
 function clearBoard() {
